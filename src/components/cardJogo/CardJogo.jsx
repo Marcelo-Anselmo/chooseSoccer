42b404342c535d6bb6a67a3490a9d7ec200f65ea
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-11-11 00:37:14 -0300

    FIX: Hooks Render

--- src/components/cardJogo/CardJogo.jsx
+++ src/components/cardJogo/CardJogo.jsx
@@ -6,38 +6,39 @@ import CardDetailShow from '../cardDetailShow/CardDetailShow';
 import useFetch from '../../hooks/useFetch';
 
 export default function CardJogo() {
-  const [isVisible, setIsVisible] = useState(false)
+  const [isVisible, setIsVisible] = useState(false);
 
-  const { data, isFetching } = useFetch('https://choose-soccer-backend.vercel.app/')
+  const { data, isFetching } = useFetch(
+    "https://choose-soccer-backend.vercel.app/"
+  );
+
+  const renderContent = () => {
+    if (isFetching) {
+      return <p>CARREGANDO..</p>;
+    } else if (data) {
+      return (
+        <>
+          <h1>Jogos da rodada {data.rodada}</h1>
+          <p>{data.clubes[262].abreviacao}</p>
+          <div
+            className="cardgeral"
+            onMouseEnter={() => setIsVisible(true)}
+            onMouseLeave={() => setIsVisible(false)}>
+            <section className="cardjogo">{/* Seu código existente */}</section>
+            <section className="cardinfo">
+              {!isVisible ? <CardDetail /> : <CardDetailShow />}
+            </section>
+          </div>
+        </>
+      );
+    } else {
+      return null; // Lidar com o caso em que o `data` ainda não está disponível
+    }
+  };
 
   return (
-    <>
-      <div style={{ backgroundColor: "aliceblue", padding: "1rem" }}>
-        <h1>Jogos da rodada {data.rodada}</h1>
-        {isFetching && <p>CARREGANDO..</p>}
-      </div>
-      <div
-        className="cardgeral"
-        onMouseEnter={() => setIsVisible(true)}
-        onMouseLeave={() => setIsVisible(false)}>
-        <section className="cardjogo">
-          <div className="cardjogo__aside cardjogo__asideleft">
-            <p>RMA</p>
-            <h2>Universidad Catolica do Chile escambau</h2>
-            <img src="time01.svg" alt="" />
-          </div>
-          <div className="cardjogo__aside cardjogo__asideright">
-            <p>CITY</p>
-            <h2>manchester city</h2>
-            <img src="time02.svg" alt="" />
-          </div>
-          <div className="placarflutuante">1 x 9</div>
-        </section>
-        <section className="cardinfo">
-          {!isVisible ? <CardDetail /> : <CardDetailShow />}
-        </section>
-      </div>
-    </>
+    <div style={{ backgroundColor: "aliceblue", padding: "1rem" }}>
+      {renderContent()}
+    </div>
   );
 }
-
