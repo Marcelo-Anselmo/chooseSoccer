829c7b98ce62c507de77a131e41e233261a264a3
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-11-12 23:25:32 -0300

    fix:{fixtoDeploy1.1}

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,12 +1,12 @@
 import "./index.scss";
 import CardJogo from "../../components/cardJogo/CardJogo";
-import NavBar from "../../components/navBar/Navbar";
+// import NavBar from "../../components/navBar/Navbar";
 
 export default function Home() {
   return (
     <>
       <div className="bghome">
-        <NavBar />
+        {/* <NavBar /> */}
         <div className="home">
           <CardJogo />
         </div>
